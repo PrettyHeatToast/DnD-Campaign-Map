736b8bf6dd6b0abec3aec9a06c26c017ecabb881
PrettyHeatToast <wouthofman01@gmail.com>
2024-06-01 15:54:59 +0200

    Improved error logging

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
 // main.js
 
-document.addEventListener("DOMContentLoaded", function () {
-    var slider = document.getElementById("dayRange");
-    var output = document.getElementById("sliderValue");
-    output.innerHTML = "Dag " + slider.value;
+var slider = document.getElementById("dayRange");
+var output = document.getElementById("sliderValue");
+output.innerHTML = "Dag " + slider.value;
 
-    slider.oninput = function () {
-        output.innerHTML = "Dag " + this.value;
-        loadMarkers(this.value);
-    }
+slider.oninput = function () {
+    output.innerHTML = "Dag " + this.value;
+    loadMarkers(this.value);
+};
 
-    // Initial load
-    loadMarkers(slider.value);
-});
+function loadMarkers(day) {
+    fetch(`/json/ennon/dag${day}.json`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => addMarkersFromJSON(data))
+        .catch(error => console.error('Error fetching data:', error));
+}
+
+// Initial load
+loadMarkers(slider.value);
